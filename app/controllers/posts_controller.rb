95ae38f792223f9769e8ae37ea53361af520f1fd
山内　英夫 <hideo_yamauchi@waku-2.com>
2015-06-02 16:22:12 +0900

    test

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,24 @@
+class PostsController < ApplicationController
+  # GET /posts/new
+  def new
+  end
+
+  # POST /posts
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to @post
+    # render text: params[:post].inspect
+  end
+
+  #GET /posts/:id
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :text)
+    end
+
+end
